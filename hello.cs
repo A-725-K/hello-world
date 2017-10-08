4dc7254ae801d78b670606695dca4a5d8947c595
A-725-K <32621046+A-725-K@users.noreply.github.com>
2017-10-08 22:25:58 +0200

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApplication {
+    using System;
+    
+    using static System.Console;
+    
+    public class Program {
+        public static void Main(/*string[] args*/) {
+            WriteLine("Hello World!");
+            ReadLine();
+        }
+    }
+}
